9313e28430348c73ed53c01908ff3fd9731956f2
Joao Vitor <githubconta2021@gmail.com>
2025-04-23 09:57:33 -0300

    Description: QUERY FILTRAR O VOLUME DE VENDA POR LOJA

--- Comando SQL/comandos.sql
+++ Comando SQL/comandos.sql
@@ -81,3 +81,21 @@ GROUP BY marca
 ORDER BY "Vendas (#)" DESC
 LIMIT 5;  
 
+
+
+
+-- (QUERY 3) Loja que mais venderam no mês  
+-- Coluna: marca, vendas (#)
+
+SELECT 
+    sto.store_name AS loja, 
+	COUNT(fun.paid_date) AS "Vendas (#)"
+FROM sales.funnel AS fun 
+LEFT JOIN sales.stores AS sto 
+     ON fun.store_id = sto.store_id
+WHERE paid_date BETWEEN '2021-08-01' AND '2021-08-31'	 
+GROUP BY loja
+ORDER BY "Vendas (#)" DESC
+LIMIT 5;  
+
+
